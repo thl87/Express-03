f39cbf7fa6943891eefd2ac7974b76a23b4b24a5
thl87 <102423538+thl87@users.noreply.github.com>
2022-11-07 14:11:35 +0100

    Update movieHandlers.js

--- movieHandlers.js
+++ movieHandlers.js
@@ -28,10 +28,6 @@ const movies = [
 const database = require("./database");
 
 
-/*const getMovies = (req, res) => {
-  res.json(movies);
-};*/
-
 const getMovies = (req, res) => {
   database
     .query("select * from movies")
@@ -63,17 +59,6 @@ const getMovieById = (req, res) => {
     
 };
 
-/*const getMovieById = (req, res) => {
-  const id = parseInt(req.params.id);
-
-  const movie = movies.find((movie) => movie.id === id);
-
-  if (movie != null) {
-    res.json(movie);
-  } else {
-    res.status(404).send("Not Found");
-  }
-};*/
 
 const getUsers = (req, res) => {
   database
